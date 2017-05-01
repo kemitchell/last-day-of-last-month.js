1614f64229a7747a7bfacabf5af62ecb4c32333c
Christian Bundy <christianbundy@fraction.io>
2017-05-01 14:49:02 -0700

    Simplify time change with setDate() and setHours()
    Replace `setHours(-24)` with `setDate(n-1)`, and use optional parameters to `setHours()` to set hours, minutes, seconds, and milliseconds.

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 module.exports = function () {
   var returned = new Date()
-  // Set day back to the first of this month.
-  returned.setDate(1)
-  // Move back 24 hours, leaving us at the current time on the last day
-  // of the last month.
-  returned.setHours(-24)
+  // Set day back to the last day of last month.
+  returned.setDate(0)
   // Zero out time components.
-  returned.setHours(0)
-  returned.setMinutes(0)
-  returned.setSeconds(0)
-  returned.setMilliseconds(0)
+  returned.setHours(0, 0, 0, 0)
   return returned
 }
